f28a493f2718e9a67306c2f9ddaf1030c431c8cc
R-T-B <21GunSoftware@comcast.net>
2024-12-29 19:12:04 -0800

    Switch to ingame patching.

---
+++ KAC/KAC.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace KAC
+{
+    [KSPAddon(KSPAddon.Startup.Instantly, true)]
+    public class KAC : MonoBehaviour
+    {
+        public static bool patched;
+        private void Awake()
+        {
+            String dllPath = "";
+            if (Application.platform == RuntimePlatform.WindowsPlayer)
+            {
+                dllPath = KSPUtil.ApplicationRootPath + "KSP_x64_Data/Managed/Assembly-CSharp.dll";
+            }
+            else if (Application.platform == RuntimePlatform.OSXPlayer)
+            {
+                dllPath = KSPUtil.ApplicationRootPath + "KSP.app/Contents/Resources/Data/Managed/Assembly-CSharp.dll";
+            }
+            else if (Application.platform == RuntimePlatform.LinuxPlayer)
+            {
+                dllPath = KSPUtil.ApplicationRootPath + "KSP_Data/Managed/Assembly-CSharp.dll";
+            }
+            if (File.Exists(dllPath))
+            {
+                Byte[] data = File.ReadAllBytes(dllPath);
+                if (data.Length >= 10485760)
+                {
+                    if ((!KAC.patched) && (Application.platform == RuntimePlatform.WindowsPlayer))
+                    {
+                        new Process
+                        {
+                            StartInfo =
+                    {
+                        UseShellExecute = true,
+                        FileName = "autoexec_win.cmd",
+                        CreateNoWindow = true,
+                        WorkingDirectory = KSPUtil.ApplicationRootPath + "/GameData/KAC/"
+                    }
+                        }.Start();
+                        KAC.patched = true;
+                    }
+                    else if ((!KAC.patched) && (Application.platform == RuntimePlatform.OSXPlayer))
+                    {
+                        new Process
+                        {
+                            StartInfo =
+                    {
+                        UseShellExecute = true,
+                        FileName = "autoexec_mac.sh",
+                        CreateNoWindow = true,
+                        WorkingDirectory = KSPUtil.ApplicationRootPath + "/GameData/KAC/"
+                    }
+                        }.Start();
+                        KAC.patched = true;
+                    }
+                    else if ((!KAC.patched) && (Application.platform == RuntimePlatform.LinuxPlayer))
+                    {
+                        new Process
+                        {
+                            StartInfo =
+                    {
+                        UseShellExecute = true,
+                        FileName = "autoexec_lin.sh",
+                        CreateNoWindow = true,
+                        WorkingDirectory = KSPUtil.ApplicationRootPath + "/GameData/KAC/"
+                    }
+                        }.Start();
+                        KAC.patched = true;
+                    }
+                    PopupDialog.SpawnPopupDialog(new Vector2(0.5f, 0.5f), new Vector2(0.5f, 0.5f), "KAC Patcher", "KAC Patcher", "Assembly-CSharp.dll patched!  We also made a .bak backup of your original.  Please quit and relaunch for it to work!", "OK", true, UISkinManager.defaultSkin);
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # KAC - Kerbal Assembly-CSharp.dll Cleaner
-A windows mode patchset for deobfuscating Assembly-CSharp.dll (may also work in WINE on other platforms, or you can run de4dot --dont-rename "Path/to/assembly-CSharp.dll" on your native platform).
+A in-game patchset for deobfuscating Assembly-CSharp.dll, which can have small performance benefits.  It runs once, patches your assembly and then closes forever.
 
-The project files are a from a propietary binary patcher to avoid legal issues (Indigo Rose Visual Patch).  The exe releases are also from that program.  They are easy to use, download your KSP version and put in the KSP directory root.  Run and enjoy!
+Non-windows users should have mono installed.
+
+https://www.mono-project.com/download/stable/
